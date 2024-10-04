684e3baeaee8d2fcdac74a041d8a96d776d004d5
arielalfaroalvarez <20235058@esen.edu.sv>
2024-10-03 21:07:54 -0600

    Navbar responsivo

--- js/main.js
+++ js/main.js
@@ -1,28 +1,16 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var menulista = document.getElementById("menulista");
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+menulista.style.maxHeight = "0px";
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+function togglemenu(){
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    if(menulista.style.maxHeight == "0px")
+    {
+        menulista.style.maxHeight = "400px";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    else
+    {
+        menulista.style.maxHeight = "0px";
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+
 }
